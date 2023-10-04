63e9c5bff53a6305b734f3392a5ff8dbf08ec1f3
CherHamza <hamzacheracher1@hotmail.fr>
2023-10-04 12:59:17 +0200

    crud done on cards & terms. still working on the ergonomy

--- src/components/Memo.jsx
+++ src/components/Memo.jsx
@@ -4,18 +4,22 @@ import JsonServerColumns from "../services/JsonServerColumns";
 import JsonServerCards from "../services/JsonServerCards";
 
 function Memo() {
-  const [terms, setTerms] = useState([]);
-  const [columns, setColumns] = useState([]);
-  const [cards, setCards] = useState([]);
+  // Définition des états du composant
+  const [terms, setTerms] = useState([]); // Liste des termes
+  const [columns, setColumns] = useState([]); // Liste des colonnes
+  const [cards, setCards] = useState([]); // Liste des cartes
   const [newCard, setNewCard] = useState({
     question: "",
     answer: "",
     column: "",
     term: ""
-  });
-  const [selectedTerm, setSelectedTerm] = useState(""); // afin de mettre a jour  l'etat pour le terme selectionné
+  }); // État du formulaire pour ajouter une carte
+  const [selectedTerm, setSelectedTerm] = useState(""); // Terme selectionné
+  const [newTerm, setNewTerm] = useState(""); // new terme à ajouter
 
+  // UseEffect pour charger les donnés du composant
   useEffect(() => {
+    // Fonction async pour charger les termes depuis JsonServer
     const fetchTerms = async () => {
       try {
         const loadedTerms = await JsonServerTerms.loadTerms();
@@ -25,6 +29,7 @@ function Memo() {
       }
     };
 
+    // Fonction async pour charger les colonnes depuis le Jsonserveur
     const fetchColumns = async () => {
       try {
         const loadedColumns = await JsonServerColumns.loadColumns();
@@ -34,6 +39,7 @@ function Memo() {
       }
     };
 
+    // Fonction asynchrone pour charger les cartes depuis le serveur
     const fetchCards = async () => {
       try {
         const loadedCards = await JsonServerCards.loadCards();
@@ -43,51 +49,104 @@ function Memo() {
       }
     };
 
+    // Appeler les fonctions pour charger les données
     fetchTerms();
     fetchColumns();
     fetchCards();
   }, []);
 
+  // Gérer le clic sur un terme
   const handleTermClick = (term) => {
-    setSelectedTerm(term.id); // afin de mettre a jour le terme selectionné
+    setSelectedTerm(term.id); // Mettre à jour le terme sélectionné
   };
 
+  // Fonction pour ajouter un terme
+  const handleAddTerm = async () => {
+    try {
+      const addedTerm = await JsonServerTerms.addTerm({
+        name: newTerm
+      });
+      setTerms([...terms, addedTerm]); // Ajouter le terme à la liste
+      setNewTerm(""); // Réinitialiser le champ du nom du terme
+    } catch (error) {
+      console.error("Erreur lors de l'ajout du terme :", error);
+    }
+  };
+
+  // Fonction pour supprimer un terme
+  const handleDeleteTerm = async (termId) => {
+    try {
+      await JsonServerTerms.deleteTerm(termId);
+      setTerms(terms.filter((term) => term.id !== termId)); // Filtrer les termes pour supprimer celui avec l'ID donné
+    } catch (error) {
+      console.error("Erreur lors de la suppression du terme :", error);
+    }
+  };
+
+  // Filtrer les cartes en fonction de la colonne et du terme sélectionnés
   const getCardsByColumnIdAndTerm = (columnId, termId) => {
     return cards.filter(
       (card) => card.column === columnId && (termId === "" || card.tid === termId)
     );
   };
 
+  // Fonction pour ajouter une carte
   const handleAddCard = async () => {
     try {
       const addedCard = await JsonServerCards.addCard({
         ...newCard,
-        term: selectedTerm // Utilise le terme sélectionné
+        term: selectedTerm // Utiliser le terme sélectionné
       });
-      setCards([...cards, addedCard]);
+      setCards([...cards, addedCard]); // Ajouter la carte à la liste
       setNewCard({
         question: "",
         answer: "",
         column: "",
         term: ""
-      });
+      }); // Réinitialiser le formulaire d'ajout de carte
     } catch (error) {
       console.error("Erreur lors de l'ajout de la carte :", error);
     }
   };
 
+  // Fonction pour supprimer une carte
   const handleDeleteCard = async (cardId) => {
     try {
       await JsonServerCards.deleteCard(cardId);
-      setCards(cards.filter((card) => card.id !== cardId));
+      setCards(cards.filter((card) => card.id !== cardId)); // Filtrer les cartes pour supprimer celle avec l'ID donné
     } catch (error) {
       console.error("Erreur lors de la suppression de la carte :", error);
     }
   };
 
+  // Rendu du composant
   return (
     <div>
       <h1>MEMO</h1>
+
+      {/* Champ pour le nom du nouveau terme */}
+      <div className="mb-3">
+        <label htmlFor="newTerm" className="form-label">
+          Nouveau terme
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="newTerm"
+          value={newTerm}
+          onChange={(e) => setNewTerm(e.target.value)}
+        />
+      </div>
+
+      {/* Bouton pour ajouter un terme */}
+      <button
+        className="btn btn-success mb-3"
+        onClick={handleAddTerm}
+      >
+        Ajouter un terme
+      </button>
+
+      {/* Liste des termes */}
       <div className="d-flex justify-content-around">
         {terms.length > 0 &&
           terms.map((term) => (
@@ -99,84 +158,86 @@ function Memo() {
               onClick={() => handleTermClick(term)}
             >
               {term.name}
+              <button onClick={() => handleDeleteTerm(term.id)}>Supprimer</button>
             </span>
           ))}
       </div>
+
+      {/* Formulaire pour ajouter une carte */}
       <div>
-  <h3>Ajouter une carte</h3>
-  <form>
-    <div className="mb-3">
-      <label htmlFor="question" className="form-label">
-        Question
-      </label>
-      <input
-        type="text"
-        className="form-control"
-        id="question"
-        value={newCard.question}
-        onChange={(e) => setNewCard({ ...newCard, question: e.target.value })}
-        required
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="answer" className="form-label">
-        Réponse
-      </label>
-      <input
-        type="text"
-        className="form-control"
-        id="answer"
-        value={newCard.answer}
-        onChange={(e) => setNewCard({ ...newCard, answer: e.target.value })}
-        required
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="column" className="form-label">
-        Colonne
-      </label>
-      <select
-        className="form-select"
-        id="column"
-        value={newCard.column}
-        onChange={(e) => setNewCard({ ...newCard, column: parseInt(e.target.value) })}
-        required
-      >
-        <option value="">Sélectionnez une colonne</option>
-        {columns.map((col) => (
-          <option key={col.id} value={col.id}>
-            {col.label}
-          </option>
-        ))}
-      </select>
-    </div>
-    <div className="mb-3">
-      <label htmlFor="term" className="form-label">
-        Terme
-      </label>
-      <select
-        className="form-select"
-        id="term"
-        value={newCard.tid} // 
-        onChange={(e) => setNewCard({ ...newCard, tid: parseInt(e.target.value) })}
-        required
-      >
-        <option value="">Sélectionnez un terme</option>
-        {terms.map((term) => (
-          <option key={term.id} value={term.id}>
-            {term.name}
-          </option>
-        ))}
-      </select>
-    </div>
-    <button type="button" className="btn btn-primary" onClick={handleAddCard}>
-      Ajouter
-    </button>
-  </form>
-</div>
+        <h3>Ajouter une carte</h3>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="question" className="form-label">
+              Question
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="question"
+              value={newCard.question}
+              onChange={(e) => setNewCard({ ...newCard, question: e.target.value })}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="answer" className="form-label">
+              Réponse
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="answer"
+              value={newCard.answer}
+              onChange={(e) => setNewCard({ ...newCard, answer: e.target.value })}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="column" className="form-label">
+              Colonne
+            </label>
+            <select
+              className="form-select"
+              id="column"
+              value={newCard.column}
+              onChange={(e) => setNewCard({ ...newCard, column: parseInt(e.target.value) })}
+              required
+            >
+              <option value="">Sélectionnez une colonne</option>
+              {columns.map((col) => (
+                <option key={col.id} value={col.id}>
+                  {col.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="term" className="form-label">
+              Terme
+            </label>
+            <select
+              className="form-select"
+              id="term"
+              value={newCard.tid}
+              onChange={(e) => setNewCard({ ...newCard, tid: parseInt(e.target.value) })}
+              required
+            >
+              <option value="">Sélectionnez un terme</option>
+              {terms.map((term) => (
+                <option key={term.id} value={term.id}>
+                  {term.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button type="button" className="btn btn-primary" onClick={handleAddCard}>
+            Ajouter
+          </button>
+        </form>
+      </div>
 
-
-
+      {/* Affichage des colonnes et des cartes */}
       <div>
         <h2>Colonnes</h2>
         <div className="d-flex justify-content-around">
--- src/services/JsonServerTerms.jsx
+++ src/services/JsonServerTerms.jsx
@@ -16,5 +16,42 @@ export default class JsonServerTerms {
           console.error("Erreur attrapée dans loadTerms : " + error);
         });
     }
+    static async addTerm(newTerm) {
+      try {
+        const response = await fetch(JsonServerTerms.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newTerm),
+        });
+  
+        if (!response.ok) {
+          throw new Error("Erreur lors de l'ajout du terme");
+        }
+  
+        const addedTerm = await response.json();
+        return addedTerm;
+      } catch (error) {
+        console.error("Erreur attrapée dans addTerm : " + error);
+        throw error;
+      }
+    }
+  
+    static async deleteTerm(termId) {
+      try {
+        const response = await fetch(`${JsonServerTerms.url}/${termId}`, {
+          method: "DELETE",
+        });
+  
+        if (!response.ok) {
+          throw new Error("Erreur lors de la suppression du terme");
+        }
+      } catch (error) {
+        console.error("Erreur attrapée dans deleteTerm : " + error);
+        throw error;
+      }
+    }
+  
   }
   
